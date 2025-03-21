7578a543cf5deff76c1c533fe408641a90d0d4cb
Jahidul Islam <jahid.redmi10@gmail.com>
2025-03-22 00:16:15 +0600

    update

--- src/components/athentication/register/Register.jsx
+++ src/components/athentication/register/Register.jsx
@@ -1,9 +1,20 @@
+import { useForm } from "react-hook-form";
 
 
 const Register = () => {
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data)
     return (
-        <div>
-            <h2>this is register page </h2>
+        <div className="">
+<div className="mt-[200px]">
+<form onSubmit={handleSubmit(onSubmit)}>
+      <input className="w-full bg-red-700" {...register("name")} />
+      <input className="w-full bg-red-700" {...register("email")} />
+      <input className="w-full bg-red-700" {...register("password")} />
+  
+      <input type="submit" />
+    </form>
+</div>
         </div>
     );
 };
